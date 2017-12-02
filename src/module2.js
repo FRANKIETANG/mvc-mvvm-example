c688cc6c4e30fc12d681fba7a1cc98dd8f097b97
FRANKIETANG <350558468@qq.com>
2017-12-02 23:51:32 +0800

    render

--- src/module2.js
+++ src/module2.js
@@ -1,13 +1,27 @@
 import Controller from './Controller'
+import $ from 'jquery'
 
 new Controller({
     element: '.module2',
     events: {
-        'change input': e => {
-            console.log('change2')
-        },
-        'click button': e => {
-            console.log('click2')
+        'change input': 'onChangeInput',
+        'click button': 'onClickButton'
+    },
+    onClickButton(e) {
+        let value = this.$element.find('input').val()
+        this.render(value)
+    },
+    onChangeInput(e) {
+        let input = e.currentTarget
+        console.log(input.value)
+    },
+    render(value) {
+        let $output = this.$element.find('.output')
+        if ($output.length === 0) {
+            $output = $('<div class="output"></div>').text(value)
+            $output.appendTo(this.$element)
+        } else {
+            $output.text(value)
         }
     }
 })
