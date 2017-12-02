1acc7f607afe0ccc80443245b0a0f5d3beddfa5e
FRANKIETANG <350558468@qq.com>
2017-12-02 20:49:18 +0800

    modules-test

--- src/module1.js
+++ src/module1.js
@@ -1,11 +1,38 @@
 import $ from 'jquery'
 
 /*module1*/
-let $module1 = $('.module1')
+// let $module1 = $('.module1')
 
-$module1.on('change', 'input', ()=>{
-    console.log('change1')
-})
-$module1.on('click', 'button', ()=>{
-    console.log('click1')
+// $module1.on('change', 'input', ()=>{
+//     console.log('change1')
+// })
+// $module1.on('click', 'button', ()=>{
+//     console.log('click1')
+// })
+
+function Maker(options) {
+    this.$element = $(options.element)
+    this.events = options.events
+    this.bindEvents()
+}
+
+Maker.prototype.bindEvents = function () {
+    for (let key in this.events) {
+        let parts = key.split(' ')
+        let eventType = parts.shift()
+        let selector = parts.join(' ')
+        this.$element.on(eventType, selector, this.events[key])
+    }
+}
+
+new Maker({
+    element: '.module1',
+    events: {
+        'change input': e => {
+            console.log('change1')
+        },
+        'click button': e => {
+            console.log('click1')
+        }
+    }
 })
