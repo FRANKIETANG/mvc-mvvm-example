127a6e0cf4f7390c2abf4eada84e86a5ed381583
FRANKIETANG <350558468@qq.com>
2017-12-02 21:04:44 +0800

    Controller es5 -> es6

--- src/Controller.js
+++ src/Controller.js
@@ -1,17 +1,18 @@
 import $ from 'jquery'
 
-function Controller(options) {
-    this.$element = $(options.element)
-    this.events = options.events
-    this.bindEvents()
-}
-
-Controller.prototype.bindEvents = function () {
-    for (let key in this.events) {
-        let parts = key.split(' ')
-        let eventType = parts.shift()
-        let selector = parts.join(' ')
-        this.$element.on(eventType, selector, this.events[key])
+class Controller {
+    constructor(options) {
+        this.$element = $(options.element)
+        this.events = options.events
+        this.bindEvents()
+    }
+    bindEvents() {
+        for (let key in this.events) {
+            let parts = key.split(' ')
+            let eventType = parts.shift()
+            let selector = parts.join(' ')
+            this.$element.on(eventType, selector, this.events[key])
+        }
     }
 }
 
