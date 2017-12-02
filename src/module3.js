706c669142100450aca2beb4e404f4c3e5f3984a
FRANKIETANG <350558468@qq.com>
2017-12-03 01:40:04 +0800

    control number

--- src/module3.js
+++ src/module3.js
@@ -2,12 +2,24 @@ import Controller from './Controller'
 
 new Controller({
     element: '.module3',
+    template: `
+        <button name="decrease"> - </button>
+        <span>{{number}}</span>
+        <button name="increase"> + </button>
+    `,
+    data: {
+        number: 0
+    },
     events: {
-        'change input': e => {
-            console.log('change3')
-        },
-        'click button': e => {
-            console.log('click3')
-        }
-    }
+        'click button[name=increase]': 'increase',
+        'click button[name=decrease]': 'decrease'
+    },
+    increase(){
+        this.data.number += 1
+        this.render()
+    },
+    decrease(){
+        this.data.number -= 1
+        this.render()
+    }    
 })
